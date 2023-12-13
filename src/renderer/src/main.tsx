7a45af31beaad9f4d118567769f41c9f10b18a65
AdamBurysek <114564710+AdamBurysek@users.noreply.github.com>
2023-12-13 11:56:59 +0100

    code refactoring

--- src/renderer/src/main.tsx
+++ src/renderer/src/main.tsx
@@ -2,22 +2,20 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import VidaMenu from "vida-menu";
-import HowToPage from "./components/howToPage";
-import KnowMorePage from "./components/knowMorePage";
+import HowToPage from "./components/infoPages/howToPage";
+import KnowMorePage from "./components/infoPages/knowMorePage";
 import setup from "../setup.json";
 import { HashRouter } from "react-router-dom";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
-    <>
-      <HashRouter>
-        <VidaMenu setup={setup}>
-          <App></App>
-          <HowToPage></HowToPage>
-          <KnowMorePage></KnowMorePage>
-        </VidaMenu>
-      </HashRouter>
-    </>
+    <HashRouter>
+      <VidaMenu setup={setup}>
+        <App></App>
+        <HowToPage></HowToPage>
+        <KnowMorePage></KnowMorePage>
+      </VidaMenu>
+    </HashRouter>
   </React.StrictMode>
 );
 
